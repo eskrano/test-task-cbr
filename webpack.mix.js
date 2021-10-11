aff854ac31a778e1989aa0139afd634bb297e8ae
Alex Priadko <alexp@addiedigital.com>
2021-10-11 16:57:56 +0300

    Added mix config.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,52 @@
-const mix = require('laravel-mix');
+const { join, resolve } = require('path')
+const { copySync, removeSync } = require('fs-extra')
+const mix = require('laravel-mix')
+require('laravel-mix-versionhash')
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix
+    .js('resources/js/app.js', 'public/dist/js').vue({
+    extractStyles: true
+})
+    .sass('resources/sass/app.scss', 'public/dist/css')
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .disableNotifications()
+
+if (mix.inProduction()) {
+    mix
+        // .extract() // Disabled until resolved: https://github.com/JeffreyWay/laravel-mix/issues/1889
+        // .version() // Use `laravel-mix-versionhash` for the generating correct Laravel Mix manifest file.
+        .versionHash()
+} else {
+    mix.sourceMaps()
+}
+
+mix.webpackConfig({
+    plugins: [
+        // new BundleAnalyzerPlugin()
+    ],
+    resolve: {
+        extensions: ['.js', '.json', '.vue'],
+        alias: {
+            '~': join(__dirname, './resources/js')
+        }
+    },
+    output: {
+        chunkFilename: 'dist/js/[chunkhash].js',
+        path: resolve(__dirname, mix.inProduction() ? './public/build' : './public')
+    }
+})
+
+mix.then(() => {
+    if (mix.inProduction()) {
+        process.nextTick(() => publishAseets())
+    }
+})
+
+function publishAseets () {
+    const publicDir = resolve(__dirname, './public')
+
+    removeSync(join(publicDir, 'dist'))
+    copySync(join(publicDir, 'build', 'dist'), join(publicDir, 'dist'))
+    removeSync(join(publicDir, 'build'))
+}
